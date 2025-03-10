cf3cfcc324c6b9a5a58c896099eaf8d3cc20584a
Guillaume <guillaume@rllnd.com>
2025-03-10 15:59:00 +0000

    Added code verification handling.

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,14 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Public routes that don't require authentication
-  publicRoutes: ["/", "/api/webhook", "/sign-in", "/sign-up"]
+  publicRoutes: [
+    "/", 
+    "/api/webhook", 
+    "/sign-in", 
+    "/sign-up",
+    "/sign-up/verify-email-address",
+    "/verify-email-change"
+  ]
 });
 
 export const config = {
